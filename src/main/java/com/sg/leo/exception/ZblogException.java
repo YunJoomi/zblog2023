46a2a03cc758fa7a95db5a89c31d929be8181e22
310-1 <310-1@310-27>
2023-05-25 15:10:08 +0900

    0525 스프링 수업 커밋

---
+++ src/main/java/com/sg/leo/exception/ZblogException.java
@@ -0,0 +1,11 @@
+package com.sg.leo.exception;
+
+public class ZblogException extends RuntimeException {
+	private static final long SerialVersionUID = 1L;
+	
+	public ZblogException(String message) {
+		super(message);
+	}
+	
+
+}
