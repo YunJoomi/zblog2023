3f12f76f53720f2e3618a3bfce65cd4977036894
310-1 <310-1@310-27>
2023-06-15 14:32:48 +0900

    마지막 커밋

--- src/main/java/com/sg/leo/service/UserService.java
+++ src/main/java/com/sg/leo/service/UserService.java
@@ -14,26 +14,22 @@ import com.sg.leo.repository.UserRepository;
 public class UserService {
    @Autowired
    private UserRepository userRepository;
-   
+
    @Transactional
    public void insertUser(User user) {
       user.setRole(RoleType.USER);
       userRepository.save(user);
    }
-   
-   // p116 기존사용자 조회
-   @Transactional(readOnly=true)
+
+   @Transactional(readOnly = true)
    public User getUser(String username) {
       // 검색결과가 없을때 빈 user 객체 반환
-      User findUser = userRepository.findByUsername(username).orElseGet(
-            new Supplier<User>() {
-               @Override
-               public User get() {
-                  return new User();
-               }
-            });
-      // 람다식
-      // User findUser = userRepository.findByUsername(username).orElseGet(() ->{return new User();});
+      User findUser = userRepository.findByUsername(username).orElseGet(new Supplier<User>() {
+         @Override
+         public User get() {
+            return new User();
+         }
+      });
       return findUser;
    }
 }
--- src/main/webapp/WEB-INF/blog/post/insertPost.jsp
+++ src/main/webapp/WEB-INF/blog/post/insertPost.jsp
@@ -4,15 +4,15 @@
 <div class="container mt-3">
    <form>
       <div class="mb-3 mt-3">
-         <label for="title">제목:</label> <input type="text"
-            class="form-control" id="title" placeholder="게시물제목">
+         <label for="title">제목</label> 
+         <input type="text" class="form-control" id="title" placeholder="게시물제목">
       </div>
       <div class="mb-3">
-         <label for="content class="form-label">내용:</label> 
-        <textarea class="form-control rows="10" id="content"></textarea>
+         <label for="content class="form-label">내용</label> 
+         <textarea class="form-control rows="10" id="content"></textarea>
       </div>
-      <button id="btn-insertPost" type="submit class="btn btn-primary">포스트 등록</button>
+      <button id="btn-insertPost" type="submit" class="btn btn-primary">포스트등록</button>
    </form>
 </div>
-<script src="/js/login.js"></script>
+<script src="/js/post.js"></script>
 <%@ include file="../layout/zblogfooter.jsp"%>
